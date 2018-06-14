bd6801d3a78a43486a8bff506de4e0c716f4cdfa
Toby Krieman <tobykreiman@Tobys-MacBook-Pro.local>
2018-06-14 18:07:10 -0400

    Initial Commit

---
+++ YoloDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoloDetection
+//
+//  Created by Toby Kreiman on 6/14/18.
+//  Copyright © 2018 Toby Kreiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
